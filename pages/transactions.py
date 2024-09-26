7ed026d9387f392d6956ebede176c88b0851b089
Simone Astolfi <simoasto95@gmail.com>
2024-09-26 16:08:01 +0200

    Fixed google API key

--- pages/transactions.py
+++ pages/transactions.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 import streamlit as st
-import streamlit_authenticator as stauth
 import plotly.express as px
 
 from google.oauth2.service_account import Credentials
@@ -25,27 +24,13 @@ def currency_button():
 def load_transform_data():
     ## Load data and transform data
     # Initialize Google sheet API call
-    scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-    credentials = {
-        "type": st.secrets['type'],
-        "project_id": st.secrets['project_id'],
-        "private_key_id": st.secrets['private_key_id'],
-        "private_key": st.secrets['private_key'],
-        "client_email": st.secrets['client_email'],
-        "client_id": st.secrets['client_id'],
-        "auth_uri": st.secrets['auth_uri'],
-        "token_uri": st.secrets['token_uri'],
-        "auth_provider_x509_cert_url": st.secrets['auth_provider_x509_cert_url'],
-        "client_x509_cert_url": st.secrets['client_x509_cert_url'],
-        "universe_domain": st.secrets['universe_domain']
-    }
-    creds = Credentials.from_service_account_file(credentials, scopes=scopes)
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    creds = Credentials.from_service_account_info(st.secrets['Google_API_Credentials'], scopes=scope)
     client = gspread.authorize(creds)
 
     # Connect to google sheet
     sheet_id = st.secrets['sheet_id']
     sh = client.open_by_key(sheet_id)
-    # values_list = sh.get_worksheet(0)
 
     # Extract worksheets
     wks_trans = sh.worksheet('Transactions')
