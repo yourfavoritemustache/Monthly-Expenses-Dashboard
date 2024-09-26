a3c4ccc667e81478deb928262859ef059551913d
Simone Astolfi <simoasto95@gmail.com>
2024-09-26 14:19:19 +0200

    Edit

--- pages/rebate.py
+++ pages/rebate.py
@@ -14,7 +14,7 @@ st.set_page_config(
     layout='wide',
     # initial_sidebar_state = 'collapsed',
     page_title='Monthly rebate',
-    page_icon=':material/currency_exchange',
+    # page_icon=':material/currency_exchange',
 )
 ##################################################################
 ### Data
